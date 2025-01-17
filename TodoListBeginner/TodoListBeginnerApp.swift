a42e25d526863a4a92386a95292efad5e9f1c728
tootisabz-alisina <a.haidari@tootisabz.tech>
2025-01-17 15:38:30 +0430

    Initial Commit

---
+++ TodoListBeginner/TodoListBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListBeginnerApp.swift
+//  TodoListBeginner
+//
+//  Created by Alisina Haidari on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoListBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoListBeginnerTests/TodoListBeginnerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListBeginnerTests.swift
+//  TodoListBeginnerTests
+//
+//  Created by Alisina Haidari on 17.01.2025.
+//
+
+import Testing
+@testable import TodoListBeginner
+
+struct TodoListBeginnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
